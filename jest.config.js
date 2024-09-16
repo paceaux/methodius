412c08dfa60de1ff3bacf67423b65acb9ad937f1
Frank M. Taylor <frtaylor@redhat.com>
2024-09-16 13:19:14 -0500

    removes unnec transform

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,5 @@ module.exports = {
     '(/test/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
     '**/?(*.)+(spec|test).[tj]s?(x)',
   ],
-  transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest',
-    '^.+.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.json',
-      },
-    ],
-  },
   testEnvironment: 'node',
 };

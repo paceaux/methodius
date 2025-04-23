73d8c104820e0f3e7d5e5f0b8a9eff8b89ea21e3
Frank M. Taylor <frtaylor@redhat.com>
2025-04-23 11:18:23 -0500

    Adds documented and typed levenshtein and hamming distances

--- src/functions.distances.ts
+++ src/functions.distances.ts
@@ -1,67 +1,109 @@
+import levenshtein from 'js-levenshtein';
+
 import {
   sanitizeText,
 } from './functions.tokenizers';
 
-import {getNGrams} from './functions.ngrams';
-
-type HammingDistance = {
-  differences: number;
+/**
+ * @typedef {object} HammingDistanceResult
+ * @property {number} distance - An integer representing edits needed to make one string into the other
+ * @property {number} percentDifferent - A float representing the percentage of the string that would change
+ * @property {string} [warning] - A message that will be present if the strings are not the same size and therefore result is unreliable
+ */
+type HammingDistanceResult = {
+  distance: number;
   percentDifferent: number;
+  warning?: string;
 };
 
+/**
+ * @description Finds the edit distance between two same-sized strings
+ * @param {string} string1 - A string to compare
+ * @param {string} string2 - A string to compare
+ * @param {boolean } shouldSanitize - remove diacritics and lowercase the strings
+ * @returns {HammingDistanceResult} - an object with the distance and percentage of difference
+ */
 function getHammingDistance(
-  string1: string = '',
-  string2: string = '',
-  shouldSanitize: boolean = true
-) : HammingDistance | null {
-	if (string1.length !== string2.length) {
-  	return null;
+  string1: string,
+  string2: string,
+  shouldSanitize: boolean = true,
+) : HammingDistanceResult {
+  if (typeof string1 !== 'string' || typeof string2 !== 'string') {
+    throw new Error('Both arguments must be a string.');
   }
-  
+
   const sanitizedString1 = shouldSanitize
-  	? sanitizeText(string1).trim()
+    ? sanitizeText(string1).trim()
     : string1.trim();
   const sanitizedString2 = shouldSanitize
-  	? sanitizeText(string2).trim()
+    ? sanitizeText(string2).trim()
     : string2.trim();
-    
-   let differences = 0;
-   
-   [...sanitizedString1].forEach((char, str1CharIndex) => {
-   	if (sanitizedString2[str1CharIndex] !== char) {
-    	differences = differences + 1;
-    }
-   });
 
-	const percentage = (differences / string1.length);
-	return {
-  	differences,
+  let distance = 0;
+
+  [...sanitizedString1].forEach((char, str1CharIndex) => {
+    if (sanitizedString2[str1CharIndex] !== char) {
+      distance += 1;
+    }
+  });
+
+  const percentage = (distance / string1.length);
+  const hammingDistance : HammingDistanceResult = {
+    distance,
     percentDifferent: percentage,
   };
+
+  if (sanitizedString1.length !== sanitizedString2.length) {
+    hammingDistance.warning = 'Arguments are not the same size. Because distance is based on the order of arguments, the distance is not reliable. Use another distance function.';
+  }
+  return hammingDistance;
 }
 
-function getLevenshteinDistance(string1: string, string2: string, shouldSanitize: boolean = true): number {
+/**
+ * @typedef {object} LevenshteinDistanceResult
+ * @property {number} distance - An integer representing edits needed to make one string into the other
+ * @property {Map<string, number>} percentDifferent - A map with the key being each argument, and the value is a float representing the percentage of the string that would change
+ */
+type LevenshteinDistanceResult = {
+  distance: number;
+  percentDifferent: Map<string, number>;
+};
+
+/**
+ * @description Finds the edit distance between two strings
+ * @param {string} string1 - A string to compare
+ * @param {string} string2 - A string to compare
+ * @param {boolean } shouldSanitize - remove diacritics and lowercase the strings
+ * @returns {LevenshteinDistanceResult} - an object with a distance and a map of the percentage difference of each string
+ */
+function getLevenshteinDistance(
+  string1: string = '',
+  string2: string = '',
+  shouldSanitize: boolean = true,
+) : LevenshteinDistanceResult {
+  if (typeof string1 !== 'string' || typeof string2 !== 'string') {
+    throw new Error('Both arguments must be a string.');
+  }
   const sanitizedString1 = shouldSanitize
-  	? sanitizeText(string1).trim()
+    ? sanitizeText(string1).trim()
     : string1.trim();
   const sanitizedString2 = shouldSanitize
-  	? sanitizeText(string2).trim()
+    ? sanitizeText(string2).trim()
     : string2.trim();
-  const [longerString, shorterString] = [sanitizedString1, sanitizedString2]
-    .sort((a, b) => b.length - a.length);
-  const maxDistance = longerString.length;
 
-  const ngrams1 = getNGrams(string1, 2);
-  const ngrams2 = getNGrams(string2, 2);
+  const distance = levenshtein(sanitizedString1, sanitizedString2);
+  const percentDifferent = new Map();
 
+  percentDifferent.set(sanitizedString1, distance / sanitizedString1.length);
+  percentDifferent.set(sanitizedString2, distance / sanitizedString2.length);
 
-
-
-
-    return levenshtein;
+  return {
+    distance,
+    percentDifferent,
+  };
 }
 
 export {
   getHammingDistance,
   getLevenshteinDistance,
-}
+};
--- test/ts/functions.distances.test.ts
+++ test/ts/functions.distances.test.ts
@@ -1,74 +1,132 @@
 import { describe, expect, it } from '@jest/globals';
-import {
-  getIntersection,
-  getUnion,
-  getDisjunctiveUnion,
-  getDifference
-} from '../../src/functions.comparisons';
-import {
-  getNGrams,
-  getWordNGrams,
-} from '../../src/functions.ngrams';
 
 import {
   getHammingDistance,
   getLevenshteinDistance,
 } from '../../src/functions.distances';
 
-
 describe('distancing', () => {
   describe('hamming', () => {
+    it('throws an error if both arguments are not strings', () => {
+      const string1 = 'meat';
+      expect(() => {
+        getHammingDistance(string1);
+      }).toThrow('Both arguments must be a string.');
+    });
+    it('has a warning if strings are not the same size', () => {
+      const string1 = 'meat';
+      const string2 = 'meets';
+      const hammingDistance = getHammingDistance(string1, string2, false);
+      expect(hammingDistance).toHaveProperty('warning');
+    });
     it('will get a hamming distance of two same-sized strings', () => {
-      const string1 = "meat";
-      const string2 = "meet";
+      const string1 = 'meat';
+      const string2 = 'meet';
       const hammingDistance = getHammingDistance(string1, string2);
-      expect(hammingDistance.differences).toEqual(1);
-      expect(hammingDistance.percentDifferent).toEqual(.25);
-    })
+      expect(hammingDistance.distance).toEqual(1);
+      expect(hammingDistance.percentDifferent).toEqual(0.25);
+    });
+    it('is commutative', () => {
+      const string1 = 'feet';
+      const string2 = 'feel';
+      const hammingDistance1 = getHammingDistance(string1, string2);
+      const hammingDistance2 = getHammingDistance(string2, string1);
+      expect(hammingDistance1.distance).toEqual(hammingDistance2.distance);
+    });
+    it('ignores diacritics by default', () => {
+      const string1 = 'eleve';
+      const string2 = 'élève';
+      const hammingDistance = getHammingDistance(string1, string2);
+      expect(hammingDistance.distance).toEqual(0);
+    });
+    it('can recognize diacritics and consider them edits', () => {
+      const string1 = 'eleve';
+      const string2 = 'élève';
+      const hammingDistance = getHammingDistance(string1, string2, false);
+      expect(hammingDistance.distance).toEqual(2);
+    });
     it('will optionally consider casing', () => {
-      const string1 = "meat";
-      const string2 = "Meet";
+      const string1 = 'meat';
+      const string2 = 'Meet';
       const hammingDistance = getHammingDistance(string1, string2, false);
-      expect(hammingDistance.differences).toEqual(2);
-      expect(hammingDistance.percentDifferent).toEqual(.5);
-    });
-    it('return null if they are not same length', () => {
-      const string1 = "meat";
-      const string2 = "meets";
-      const hammingDistance = getHammingDistance(string1, string2, false);
-      expect(hammingDistance).toEqual(null);
-    });
-    it('can turn off sanitization to recognize character differences', () => {
-      const string1 = "eleve";
-      const string2 = "élève";
-      const hammingDistance = getHammingDistance(string1, string2, true);
-      expect(hammingDistance).toEqual(2);
+      expect(hammingDistance.distance).toEqual(2);
+      expect(hammingDistance.percentDifferent).toEqual(0.5);
     });
   });
   describe('levenshtein', () => {
-    it('with same length, sees 1 diff', () =>{
-      const string1 = "meat";
-      const string2 = "meet";
-      const levenshtein = getLevenshteinDistance(string1, string2);
-      expect(levenshtein).toEqual(1);
+    it('throws an error if both arguments are not strings', () => {
+      const string1 = undefined;
+      const string2 = null;
+      expect(() => {
+        getLevenshteinDistance(string1, string2);
+      }).toThrow('Both arguments must be a string');
     });
-    it('meat & feet are 2 diff', () =>{
-      const string1 = "meat";
-      const string2 = "feet";
+    it('works when first string is empty', () => {
+      const string1 = 'meet';
+      const string2 = '';
       const levenshtein = getLevenshteinDistance(string1, string2);
-      expect(levenshtein).toEqual(2);
+      expect(levenshtein.distance).toEqual(4);
     });
-    it('with same length, sees 3 diff', () =>{
-      const string1 = "meat";
-      const string2 = "feel";
+    it('works when second string is empty', () => {
+      const string1 = '';
+      const string2 = 'meet';
       const levenshtein = getLevenshteinDistance(string1, string2);
-      expect(levenshtein).toEqual(3);
+      expect(levenshtein.distance).toEqual(4);
     });
-    it('with same length, sees 4 diff', () =>{
-      const string1 = "meat";
-      const string2 = "fiel";
+    it('with same length, sees 1 diff', () => {
+      const string1 = 'meat';
+      const string2 = 'meet';
       const levenshtein = getLevenshteinDistance(string1, string2);
-      expect(levenshtein).toEqual(4);
+      expect(levenshtein.distance).toEqual(1);
+      expect(levenshtein.percentDifferent.get('meat')).toEqual(0.25);
+    });
+    it('meat & feet are 2 diff', () => {
+      const string1 = 'meat';
+      const string2 = 'feet';
+      const levenshtein = getLevenshteinDistance(string1, string2);
+      expect(levenshtein.distance).toEqual(2);
+      expect(levenshtein.percentDifferent.get('meat')).toEqual(0.5);
+      expect(levenshtein.percentDifferent.get('feet')).toEqual(0.5);
+    });
+    it('with same length, sees 3 diff', () => {
+      const string1 = 'meat';
+      const string2 = 'feel';
+      const levenshtein = getLevenshteinDistance(string1, string2);
+      expect(levenshtein.distance).toEqual(3);
+    });
+    it('with same length, sees 4 diff', () => {
+      const string1 = 'meat';
+      const string2 = 'fiel';
+      const levenshtein = getLevenshteinDistance(string1, string2);
+      expect(levenshtein.distance).toEqual(4);
+    });
+    it('is commutative when arguments are same sizes', () => {
+      const string1 = 'feet';
+      const string2 = 'meet';
+      const levenshtein1 = getLevenshteinDistance(string1, string2);
+      const levenshtein2 = getLevenshteinDistance(string2, string1);
+      expect(levenshtein1.distance).toEqual(1);
+      expect(levenshtein1.distance).toEqual(levenshtein2.distance);
+    });
+    it('is commutative when arguments are different sizes', () => {
+      const string1 = 'feeling';
+      const string2 = 'meet';
+      const levenshtein1 = getLevenshteinDistance(string1, string2);
+      const levenshtein2 = getLevenshteinDistance(string2, string1);
+      expect(levenshtein1.distance).toEqual(levenshtein2.distance);
+    });
+
+    it('ignores diacritics by default', () => {
+      const string1 = 'résumé';
+      const string2 = 'resume';
+      const levenshtein = getLevenshteinDistance(string1, string2);
+      expect(levenshtein.distance).toEqual(0);
+    });
+    it('can recognize diacritics and consider them edits', () => {
+      const string1 = 'résumé';
+      const string2 = 'resume';
+      const levenshtein = getLevenshteinDistance(string1, string2, false);
+      expect(levenshtein.distance).toEqual(2);
     });
   });
-})
+});
